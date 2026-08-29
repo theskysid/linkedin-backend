0096cdcaa5a92dc0db3c08e9eff6a1995d72e8f1
theskysid <23x10sid@gmail.com>
2026-08-29 18:32:08 +0530

    All micro-services intitalized

---
+++ post-service/src/test/java/com/linkedin/postservice/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}

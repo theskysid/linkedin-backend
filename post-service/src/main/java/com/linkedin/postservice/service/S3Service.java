8d8595cf3a76f54b462da93293975fa9f8ae7184
theskysid <23x10sid@gmail.com>
2026-09-02 14:08:22 +0530

    s3service and config and yml configuration

--- post-service/src/main/java/com/linkedin/postservice/service/S3Service.java
+++ post-service/src/main/java/com/linkedin/postservice/service/S3Service.java
@@ -1,8 +1,49 @@
 package com.linkedin.postservice.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.util.UUID;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class S3Service {
+
+   private final S3Client s3Client;
+
+   @Value("${aws.s3.bucket-name}")
+   private String bucketName;
+
+   @Value("${aws.region}")
+   private String region;
+
+   public String uploadFile(MultipartFile file, String keyPrefix) {
+      try {
+         String key = keyPrefix + "/" +  UUID.randomUUID() + "_" +  file.getOriginalFilename();
+
+         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                 .bucket(bucketName)
+                 .key(key)
+                 .contentType(file.getContentType())
+                 .build();
+
+         s3Client.putObject(request, RequestBody.fromBytes(file.getBytes()));
+
+         String url = "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + key;
+
+         log.info("File Uploaded Successfully to s3: {} " + url);
+
+      } catch (Exception e) {
+
+         throw new RuntimeException("Failed to upload file to s3", e.getMessage());
+      }
+   }
 
 }
